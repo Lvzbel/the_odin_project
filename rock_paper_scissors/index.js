901c529c70b4229673844de6790a788b01bf38c8
Rodrigo <rcoto@protonmail.com>
2019-01-28 14:56:21 -0800

    Rock Paper Scissors has now a visual interface

--- rock_paper_scissors/index.js
+++ rock_paper_scissors/index.js
@@ -1,3 +1,13 @@
+const optionButtons = document.querySelectorAll(".btn-secondary");
+const message = document.querySelector("#message");
+const playerScore = document.querySelector("#playerScore");
+const computerScore = document.querySelector("#computerScore");
+const resetButton = document.querySelector("#reset");
+
+// Score Keeping;
+let wins = 0;
+let losses = 0;
+
 // Computer ramdomized selection
 const computerPlay = () => {
   const options = ["rock", "paper", "scissors"];
@@ -6,56 +16,60 @@ const computerPlay = () => {
 };
 
 const playRound = (playerSelection, computerSelection) => {
-  const player = playerSelection.toLowerCase();
+  const player = playerSelection;
   const computer = computerSelection;
 
   // Ways to lose when player picks Rock
   if (player === "rock" && computer === "paper") {
-    return `You Lose! ${computer} beats ${player}`;
+    return `You Lose!`;
     // Ways to win when player picks Rock
   } else if (player === "rock" && computer === "scissors") {
-    return `You Win! ${player} beats ${computer}`;
+    return `You Win!`;
     // Player lose with paper pick
   } else if (player === "paper" && computer === "scissors") {
-    return `You Lose! ${computer} beats ${player}`;
+    return `You Lose!`;
     // Player wins with paper pick
   } else if (player === "paper" && computer === "rock") {
-    return `You Win! ${player} beats ${computer}`;
+    return `You Win!`;
     // Player lose with scissors pick
   } else if (player === "scissors" && computer === "rock") {
-    return `You Lose! ${computer} beats ${player}`;
+    return `You Lose!`;
     // Player wins with scissors pick
   } else if (player === "scissors" && computer === "paper") {
-    return `You Win! ${player} beats ${computer}`;
+    return `You Win!`;
   } else {
     return "Its a draw!";
   }
 };
 
-const game = () => {
-  let wins = 0;
-  let losses = 0;
-  let gameEnd = true;
-  // Loop will run until a best of 5 is decided
-  while (gameEnd) {
-    const player = window.prompt("Make your pick: Rock, Paper or Scissors");
-    const computer = computerPlay();
-    const result = playRound(player, computer);
-    if (result.includes("Win!")) {
-      wins++;
-    } else if (result.includes("Lose!")) {
-      losses++;
-    }
-
-    if (wins === 3 || losses === 3) {
-      gameEnd = false;
-    }
-    console.log(result);
-    console.log(`Wins: ${wins} and Losses: ${losses}`);
+const game = playerSelection => {
+  const player = playerSelection;
+  const computer = computerPlay();
+  const result = playRound(player, computer);
+  if (result.includes("Win!")) {
+    wins++;
+  } else if (result.includes("Lose!")) {
+    losses++;
   }
 
-  wins === 3 ? console.log("You Won!") : console.log("You Lose!");
-  console.log(`With a score of You: ${wins} vs Computer: ${losses}`);
+  message.textContent = result;
+  playerScore.textContent = wins;
+  computerScore.textContent = losses;
 };
 
-game();
+// EventListerners
+optionButtons.forEach(button => {
+  button.addEventListener("click", e => {
+    // Start game with selection
+    game(e.target.id);
+  });
+});
+
+// Reset Button
+resetButton.addEventListener("click", () => {
+  wins = 0;
+  losses = 0;
+  playerScore.textContent = "0";
+  computerScore.textContent = "0";
+  message.textContent = "";
+});
