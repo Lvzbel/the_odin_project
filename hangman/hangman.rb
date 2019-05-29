7e4e3f8688dee02027e5c131552ab5530f6ffd56
Rodrigo <rcoto@protonmail.com>
2019-05-28 17:26:59 -0700

    feat: function that picks a ramdon word is completed

--- hangman/hangman.rb
+++ hangman/hangman.rb
@@ -4,4 +4,17 @@ puts "Game has started"
 dictionary_txt = File.read "5desk.txt"
 dictionary = dictionary_txt.split(/\n/)
 
-p dictionary[55].gsub(/\r/, "")
+# Select a ramdon word between 5 and 12 chars long
+def ramdon_word(words_array)
+  random_number = rand(0..words_array.length)
+  word = words_array[random_number].gsub!(/\r/, "")
+  p word
+  if word.length >= 5 && word.length <= 12
+    return word.downcase
+  else
+    puts "Looking for another word"
+    ramdon_word(words_array)
+  end
+end 
+
+p ramdon_word(dictionary)
