4f433f3c9265c7f6a66725d360f3837e8c353478
Rodrigo <rcoto@protonmail.com>
2019-05-28 20:02:39 -0700

    feat: input loop in currently working and know if a incorrect input in provided

--- hangman/hangman.rb
+++ hangman/hangman.rb
@@ -1,4 +1,4 @@
-end_game = true
+end_game = false
 
 # Load and convert txt file into an array
 dictionary_txt = File.read "5desk.txt"
@@ -13,18 +13,36 @@ def ramdon_word(words_array)
   else
     ramdon_word(words_array)
   end
-end 
+end
+
+def deep_copy(o)
+  Marshal.load(Marshal.dump(o))
+end
 
 new_status = {
   remaining_guesses: 5,
   wrong_guesses: 0,
   word: ramdon_word(dictionary).split(""),
-  guessed_letters: []
+  guessed_letters: [],
+  right_guesses: []
 }
 
-p new_status[:word]
+current_status = deep_copy(new_status)
 
 # Game loop
 until end_game
-  puts "Hello"
+  puts "What would you like to do? Type 'exit' to quit end the game, Type 'save' to save the game and quit or type a single letter to continue with the game"
+  input = gets.chomp.downcase
+
+  if input == "exit"
+    puts "Thanks for playing, see you next time!"
+    end_game = true
+  elsif input == 'save'
+    puts "Saving game, see you next time!"
+    end_game = true
+  elsif input.length == 1 && input.match(/[a-z]/)
+    puts "valid guess"
+  else
+    puts "Invalid input please try again"
+  end
 end
